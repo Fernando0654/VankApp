bd2f5f16c788c2f598b36d515eaf9763404882f9
FernandoFeN <fernandofenosv@gmail.com>
2021-10-18 16:13:38 -0500

    color palette changed

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -5,7 +5,7 @@ const TransactionSchema = new Schema({
     correo: {type: String, required: true},
     concepto: {type: String, required: true},
     cantidad: {type: String, required: true},
-    fecha: {type: Date, default: Date.now},
+    fecha: {type: Date, default: new Date(Date.now()).toDateString()},
     user: { type: String, required: true}
 })
 
