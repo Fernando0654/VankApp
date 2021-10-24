c8801335de917ff01cab7f47019d45910b1005a3
FernandoFeN <fernandofenosv@gmail.com>
2021-10-23 22:01:24 -0500

    Change password, logic

---
+++ src/public/js/changeP.js
@@ -0,0 +1,47 @@
+const change_password = document.getElementById("change_password");
+const change_password_container = document.querySelector(".change-password");
+let gonnaChange = false;
+change_password.addEventListener("click", () => {
+  gonnaChange = !gonnaChange;
+  if (gonnaChange) {
+    change_password_container.style.display = "block";
+    change_password.innerText = "Cancelar";
+  } else {
+    change_password_container.style.display = "none";
+    change_password.innerText = "Cambiar contraseña";
+  }
+});
+
+const enterOldPassword = document.getElementById("old_pass");
+const query = window.location.search;
+const urlParam = new URLSearchParams(query);
+const matchPassword = urlParam.get("p");
+if (matchPassword === "t") {
+  changePassword();
+} else {
+  swal("Lo sentimos", "La contraseña no coincide", "error", { button: "Continuar" });
+}
+enterOldPassword.addEventListener("keyup", async (e) => {
+  window.location.href = `/verify/${e.target.value}`;
+});
+
+function changePassword() {
+  swal({
+    content: {
+      element: "input",
+      attributes: {
+        placeholder: "Nueva contraseña",
+        type: "password",
+      },
+    },
+    button: { text: "Cambiar ahora" },
+  }).then((newPassword) => {
+    if (!newPassword) {
+      swal("Error", "El campo no puede estar vacío", "error", {
+        button: "Cerrar",
+      });
+      return;
+    }
+    window.location.href = `/changePassword/${newPassword}`;
+  });
+}
--- src/routes/config.js
+++ src/routes/config.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const { isAuth } = require("../helpers/guard");
 const User = require("../models/User");
+const bcrypt = require('bcryptjs');
 
 router.get("/configuracion", isAuth, async (req, res) => {
   const usuario = await User.findOne({ email: req.user.email });
-  const { email, name, age, phone, saldo, numero, expira, address, cvv, tipo } = usuario;
-  res.render("actions/config", {
+  const {
+    _id,
     email,
     name,
     age,
@@ -15,8 +16,79 @@ router.get("/configuracion", isAuth, async (req, res) => {
     expira,
     address,
     cvv,
-    tipo
+    tipo,
+  } = usuario;
+  res.render("actions/config", {
+    _id,
+    email,
+    name,
+    age,
+    phone,
+    saldo,
+    numero,
+    expira,
+    address,
+    tipo,
   });
 });
+
+router.get("/configuracion/editar/:id", isAuth, async (req, res) => {
+  const usuario = await User.findOne({ email: req.user.email });
+  const {
+    _id,
+    email,
+    name,
+    age,
+    phone,
+    password,
+    saldo
+  } = usuario;
+  res.render("actions/config-edit", {
+    _id,
+    email,
+    name,
+    age,
+    phone,
+    password,
+    saldo,
+  });
+});
+
+router.put("/configuracion/editar/:id", isAuth, async (req, res) => {
+  const { email, name, password, age, phone, } =
+    req.body;
+  console.log(req.params.id);
+  await User.findByIdAndUpdate(req.params.id, {
+    email,
+    name,
+    password,
+    age,
+    phone,
+  });
+  res.redirect("/configuracion");
+});
+
+router.get("/verify/:password?", isAuth, async (req, res) =>{
+  if(!req.params.password) {
+    res.redirect(`/configuracion/editar/${req.user._id}`);
+  } else {
+    const passwordEntered = req.params.password;
+    const matchPassword = await bcrypt.compare(passwordEntered, req.user.password);
+    if(matchPassword) {
+      res.redirect(`/configuracion/editar/${req.user._id}?p=t`);
+    } else {
+      res.redirect(`/configuracion/editar/${req.user._id}?p=f`);
+    }
+  }
+});
+
+router.get("/changePassword/:newPassword", isAuth, async(req, res) => {
+  const pass = req.params.newPassword;
+  const encrypted = await bcrypt.genSalt(10);
+  const newPassword = await bcrypt.hash(pass, encrypted);
+  await User.findByIdAndUpdate(req.user._id, {
+    password: newPassword
+  });
+})
 
 module.exports = router;
