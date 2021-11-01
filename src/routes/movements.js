237871115fc7ed5b8f021a9adf63b6bfc46479f9
FernandoFeN <fernandofenosv@gmail.com>
2021-10-31 18:43:21 -0600

    Styles and img changed

--- src/routes/movements.js
+++ src/routes/movements.js
@@ -3,59 +3,59 @@ const Transaction = require("../models/Transaction");
 const router = require("express").Router();
 let firstTime = true;
 
-router.get("/movimientos", isAuth, async (req, res) => {
-  let movimientos;
-  if (firstTime) {
-    movimientos = await Transaction.find({ user: req.user.id })
-      .lean()
-      .sort({ _id: -1 });
-  }
-  firtTime = false;
-  let saldo = req.user.saldo;
-  res.render("actions/seeMovements", { movimientos, saldo });
+router.get("/movimientos", isAuth, async(req, res) => {
+    let movimientos;
+    if (firstTime) {
+        movimientos = await Transaction.find({ user: req.user.id })
+            .lean()
+            .sort({ _id: -1 });
+    }
+    firtTime = false;
+    let saldo = req.user.saldo;
+    res.render("actions/seeMovements", { movimientos, saldo });
 });
 
-router.post("/movimientos/", async (req, res) => {
-  const { filtro } = req.body;
-  let movimientos;
-  switch (filtro) {
-    case "recientes":
-      movimientos = await Transaction.find({ user: req.user.id })
-        .lean()
-        .sort({ _id: -1 });
-      break;
-    case "antiguos":
-      movimientos = await Transaction.find({ user: req.user.id })
-        .lean()
-        .sort({ _id: 1 });
-      break;
-    case "cantidadAlta":
-      movimientos = await Transaction.find({ user: req.user.id })
-        .lean()
-        .sort({ cantidad: -1 });
-      break;
-    case "cantidadBaja":
-      movimientos = await Transaction.find({ user: req.user.id })
-        .lean()
-        .sort({ cantidad: 1 });
-      break;
-    default:
-      res.send("ERROR, HOLLY MOLLY YOU BROKE YOUR COMPUTER xD");
-      break;
-  }
-  let saldo = req.user.saldo;
-  res.render("actions/seeMovements", { movimientos, saldo });
+router.post("/movimientos/", async(req, res) => {
+    const { filtro } = req.body;
+    let movimientos;
+    switch (filtro) {
+        case "recientes":
+            movimientos = await Transaction.find({ user: req.user.id })
+                .lean()
+                .sort({ _id: -1 });
+            break;
+        case "antiguos":
+            movimientos = await Transaction.find({ user: req.user.id })
+                .lean()
+                .sort({ _id: 1 });
+            break;
+        case "cantidadAlta":
+            movimientos = await Transaction.find({ user: req.user.id })
+                .lean()
+                .sort({ cantidad: -1 });
+            break;
+        case "cantidadBaja":
+            movimientos = await Transaction.find({ user: req.user.id })
+                .lean()
+                .sort({ cantidad: 1 });
+            break;
+        default:
+            res.send("ERROR, HOLLY MOLLY YOU BROKE YOUR COMPUTER xD");
+            break;
+    }
+    let saldo = req.user.saldo;
+    res.render("actions/seeMovements", { movimientos, saldo });
 });
 
-router.get("/movimiento/:id", isAuth, async (req, res) => {
-  const movimiento = await Transaction.findById(req.params.id);
-  res.render("actions/seeMovement", { movimiento });
+router.get("/movimiento/:id", isAuth, async(req, res) => {
+    const movimiento = await Transaction.find({ _id: req.params.id }).lean();
+    res.render("actions/seeMovement", { movimiento });
 });
 
-router.delete("/movimiento/delete/:id", isAuth, async (req, res) => {
-  await Transaction.findByIdAndDelete(req.params.id);
-  req.flash("success_msg", "Elemento borrado");
-  res.redirect("/movimientos");
+router.delete("/movimiento/delete/:id", isAuth, async(req, res) => {
+    await Transaction.findByIdAndDelete(req.params.id);
+    req.flash("success_msg", "Elemento borrado");
+    res.redirect("/movimientos");
 });
 
 module.exports = router;
